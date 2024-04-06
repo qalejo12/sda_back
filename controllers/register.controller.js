a7def4d26f8f693680c553a081cda75dacff7301
qalejo12 <100315515+qalejo12@users.noreply.github.com>
2024-04-05 23:09:32 -0500

    back-v3-4

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -5,7 +5,6 @@ const register = async (req, res) => {
         const {name,lastname,email,age,code_teacher} = req.body;
         const response = await registerModel.registerUser(name,lastname,email,age,code_teacher);
         res.json({ message: "Usuario creado correctamente", createdUser: response });
-        res.json(response);
     } catch (error) {
         console.log(error);
     }
